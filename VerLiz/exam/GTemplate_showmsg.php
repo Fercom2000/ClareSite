baeacbca981b65f384e7dbc37b074db82b6a3415
Conejillo <carolina012299@gmail.com>
2020-01-02 11:50:46 -0600

    Version Liz added 2-1-2020

---
+++ VerLiz/exam/GTemplate_showmsg.php
@@ -0,0 +1,37 @@
+<?php
+//include ('GTemplate.html')
+
+$host       = 'localhost';
+$user       = 'root';
+$pass       = '';
+$database   = 'dbFer'; 
+
+//$users=$_POST['user'];
+//$id=$_POST['id'];
+
+
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$database", $user);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $sql="SELECT mensaje FROM `comentarios` WHERE id IN (69,70,71,72)";//id = 69 OR id=70 OR id=71 OR id=72 ";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->fetchAll();//$stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach($result as $row){//while ($row=$stmt->fetchAssoc()) {
+        echo (
+            "<tr>".
+            //"<td>".$row['mensaje']."</td>".
+           // "<td>".$row['mensaje']."</td>".
+            "<td>".$row['mensaje']."</td>"."</tr>"
+        );
+    }
+    /*foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }*/
+}catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
